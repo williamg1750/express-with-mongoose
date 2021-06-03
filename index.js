342df73a105b11965d7ae283e4919c77d7085307
williamg1750 <william.guan12@gmail.com>
2021-06-03 14:26:37 -0400

    crud

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Product = require('./models/product');
 
+//used to get pass the get and post route
+const methodOverride = require('method-override');
+//mongoose connection to teh mongo db
 mongoose
   .connect('mongodb://localhost:27017/farmStand', {
     useNewUrlParser: true,
@@ -16,11 +19,14 @@ mongoose
     console.log('OH NO MONGO CONNECTION ERROR');
     console.log(err);
   });
-
+//setting the views to work with ejs
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+//middlewares
 //need to use this mddleware to parse thru the req.body
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 app.get('/products', async (req, res) => {
   const products = await Product.find({});
@@ -49,17 +55,26 @@ app.get('/products/:id', async (req, res) => {
   res.render('products/show', { product });
 });
 
-// app.patch('/products/:id', async (req, res) => {
-//   const { id } = req.params;
-//   const product = await Product.findOneAndUpdate(id);
-
-//   res.render('products/show', { product });
-// });
-
 app.get('/products/:id/edit', async (req, res) => {
   const { id } = req.params;
-  const product = await Product.findOneAndUpdate(id);
+  const product = await Product.findById(id);
   res.render('products/edit', { product });
+});
+
+app.put('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValidators: true,
+    new: true,
+  });
+  res.redirect(`/products/${product._id}`);
+  // res.send('UR ARE PUTTING');
+});
+
+app.delete('/products/:id', async (req, res) => {
+  const { id } = req.params;
+  const deletedProduct = await Product.findByIdAndDelete(id);
+  res.redirect('/products');
 });
 
 app.get('*', async (req, res) => {
